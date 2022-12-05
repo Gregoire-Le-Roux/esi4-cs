334a9f413b8c59f791b4b3a731d529f6f710cb2e
Gregoire-Le-Roux <gregoire.leroux.pro@gmail.com>
2022-12-05 14:21:35 +0100

    Reinitialize cours project

---
+++ ESI4ConsoleApp/ESI4ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+JourSemaine aujourdhui = JourSemaine.Lundi;
+
+Console.WriteLine(aujourdhui);
+enum JourSemaine
+{
+    Lundi,
+    Mardi,
+    Mercredi,
+    Jeudi,
+    Vendredi,
+    Samedi,
+    Dimanche
+}
